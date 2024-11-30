46469701e2ca01ddb43eae1f9f5b9d5f7757f0e7
victoriamanzato <145578820+victoriamanzato@users.noreply.github.com>
2024-11-30 14:13:14 -0500

    Update board.py

--- board.py
+++ board.py
@@ -1,1 +1,16 @@
 #create the board here
+
+
+#part 2
+def clear(self):
+  self.value = 0
+  self.sketch = 0
+def sketch(self,value):
+  self.sketched_value = value
+def place_number(self,value):
+  self.value = value
+  self.sketch = 0
+def reset_to_original(self):
+  self.value = original_value
+  self.sketch = 0
+  
