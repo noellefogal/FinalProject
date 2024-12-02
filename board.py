fa6b294108a0672d01cdef4447ece23dfbb981a7
noellefogal <noellefogal@ufl.edu>
2024-12-01 23:27:39 -0500

    added constants and set up main method to test methods

--- board.py
+++ board.py
@@ -1,21 +1,33 @@
 #part 1
+from constants import *
+import pygame
+
+
+
 class Board:
-  def __init__(self, width, height, screen, difficulty):
+  def __init__(self, width, height, screen, difficulty = "EASY"):
     self.width = width
     self.height = height
     self.screen = screen
     self.difficulty = difficulty
 
+  def draw(self, screen):
+    for i in range(9):
+      pygame.draw.line(screen, BLACK, (0, i*CELL_SIZE), (BOARD_SIZE, i*CELL_SIZE))
+
+
+
+
 #part 2
-  def clear(self):
-    self.value = 0
-    self.sketch = 0
-  def sketch(self,value):
-    self.sketched_value = value
-  def place_number(self,value):
-    self.value = value
-    self.sketch = 0
-  def reset_to_original(self):
-    self.value = original_value
-    self.sketch = 0
+  #def clear(self):
+   # self.value = 0
+  #  self.sketch = 0
+ # def sketch(self,value):
+   # self.sketched_value = value
+ # def place_number(self,value):
+  #  self.value = value
+   # self.sketch = 0
+  #def reset_to_original(self):
+   # self.value = original_value
+   # self.sketch = 0
   
--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,28 @@
-#main file
+import pygame
+from constants import *
+from board import *
+
+def main():
+    try:
+        pygame.init()
+        screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
+        screen.fill("white")
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            board = Board(BOARD_SIZE, BOARD_SIZE, screen)
+            board.draw(screen)
+            pygame.display.flip()
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+    finally:
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
+
+
