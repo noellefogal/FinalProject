eebfe83be405a1b50fc033f496120deb572aa294
victoriamanzato <145578820+victoriamanzato@users.noreply.github.com>
2024-12-02 14:36:07 -0500

    Update SudokuGenerator.py

--- SudokuGenerator.py
+++ SudokuGenerator.py
@@ -1,1 +1,11 @@
 #most of the methods
+
+def valid_in_row(self,row,num):
+  return num if not self.board[row]
+
+
+def valid_in_col(self,col,num):
+  for row in range(self.row_length):
+    if self.board[row][col] == num:
+      return False
+  return True
