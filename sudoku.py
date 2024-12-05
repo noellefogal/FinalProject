7d9799f3fef1e6433496a32532630dc1a84ae49f
noellefogal <noellefogal@ufl.edu>
2024-12-04 23:03:27 -0500

    shows initial board

--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,34 @@
+from FinalProject.SudokuGenerator import SudokuGenerator
 from board import *
+from cell import *
+from SudokuGenerator import *
+import pygame, sys
 
 def main():
-    try:
-        pygame.init()
+
+        board = generate_sudoku(9, HARD)
         screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
         screen.fill(WHITE)
+        cells = [[Cell(0, row, col, screen) for col in range(9)] for row in range(9)]
+        i = 0
+        j = 0
+        for list in cells:
+            for item in list:
+                item.set_cell_value(board[i][j])
+                item.set_sketched_value(board[i][j])
+                j+=1
+            j = 0
+            i+=1
+        for list in cells:
+            for item in list:
+                item.draw()
+
+
+
+        #cells[0][0].set_cell_value(9)
+        #print(cells[0][0].value)
+
+
         clock = pygame.time.Clock()
         running = True
         while running:
@@ -14,10 +38,12 @@ def main():
             clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    pygame.quit()
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
 
-    finally:
-        pygame.quit()
+
 
 
 if __name__ == "__main__":
