ba3886da7bf203db9f08ff6fd00bbee1eddead97
noellefogal <noellefogal@ufl.edu>
2024-12-05 23:22:08 -0500

    bug fix

--- cell.py
+++ cell.py
@@ -14,6 +14,7 @@ class Cell:
         self.screen = screen
         self.sketched_value = 0
         self.selected = False
+        self.creating_board = True
 
     '''
     Sets the cell's value
@@ -32,8 +33,8 @@ class Cell:
     '''
     def draw(self):
         # Calculates the position and size of the cell
-        x = self.col * CELL_SIZE
-        y = self.row * CELL_SIZE
+        x = self.row * CELL_SIZE
+        y = self.col * CELL_SIZE
         cell_rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
 
         font = pygame.font.Font(None, FONT_SIZE)
@@ -41,18 +42,26 @@ class Cell:
         # Draw highlight if selected
         if self.selected:
             pygame.draw.rect(self.screen, RED, cell_rect, SLCTD_LINE_WIDTH)
-
         # Draw permanent value
         if self.value != 0:
+            if not self.creating_board:
+                fill_rect = pygame.Rect(x + 5, y + 5, CELL_SIZE / 2, CELL_SIZE / 2)
+                self.screen.fill(WHITE, fill_rect)
             text_surf = font.render(str(self.value), True, BLACK) # Black text
             text_rect = text_surf.get_rect(center=cell_rect.center) # Center the text within cell
             self.screen.blit(text_surf, text_rect)
+            self.creating_board = False
+
 
         # Draw sketched value
         elif self.sketched_value != 0:
             text_surf = font.render(str(self.sketched_value), True, GRAY) # Gray text
             text_rect = text_surf.get_rect(topleft=(cell_rect.x + 5, cell_rect.y + 5)) # Offset slightly from corner
             self.screen.blit(text_surf, text_rect)
+        else:
+            fill_rect = pygame.Rect(x+5, y+5, CELL_SIZE/2, CELL_SIZE/2)
+            self.screen.fill(WHITE, fill_rect)
+
 
 '''
 DELETE test_cell() BEFORE FINAL PUBLICATION !
