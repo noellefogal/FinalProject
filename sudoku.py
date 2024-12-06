c97abec8a4329c56af568eb4d5a8dee584baecee
noellefogal <noellefogal@ufl.edu>
2024-12-06 01:29:54 -0500

    can move with keys

--- sudoku.py
+++ sudoku.py
@@ -5,15 +5,19 @@ from SudokuGenerator import *
 from constants import *
 import pygame, sys
 
+
+
 def main():
         first_click = True
-        puzzle_board, solution_board = generate_sudoku(9, HARD)
         screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
         screen.fill(WHITE)
-        board = Board(BOARD_SIZE, BOARD_SIZE, screen)
+        board = Board(BOARD_SIZE, BOARD_SIZE, screen, "HARD")
         board.draw(screen)
         clock = pygame.time.Clock()
         running = True
+        x = 0
+        y = 0
+
         while running:
             pygame.display.flip()
             clock.tick(60)
@@ -28,21 +32,45 @@ def main():
                     x1,y1 = event.pos
                     x, y = board.click(x1,y1)
                     board.select(x,y)
-                    board.slctd_cell.draw()
                     first_click = False
                 elif event.type == pygame.KEYDOWN:
                     numbers = {pygame.K_1 : 1, pygame.K_2 : 2, pygame.K_3 : 3, pygame.K_4 : 4, pygame.K_5 : 5, pygame.K_6 : 6, pygame.K_7 : 7, pygame.K_8 : 8, pygame.K_9 : 9}
+                    keys = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
+                    if not first_click:
+                        for item in keys:
+                            if event.key == item:
+                                try:
+                                    ogx = x
+                                    ogy = y
+                                    if event.key == pygame.K_LEFT:
+                                        assert(0 <= x-1 <=8)
+                                        x-=1
+                                    if event.key == pygame.K_RIGHT:
+                                        assert(0 <= x + 1 <= 8)
+                                        x+=1
+                                    if event.key == pygame.K_UP:
+                                        assert(0 <= y - 1 <= 8)
+                                        y-=1
+                                    if event.key == pygame.K_DOWN:
+                                        assert(0 <= y + 1 <= 8)
+                                        y+=1
+                                    pygame.draw.rect(board.screen, WHITE,pygame.Rect(ogx * CELL_SIZE, ogy * CELL_SIZE, CELL_SIZE, CELL_SIZE),SLCTD_LINE_WIDTH)
+                                    board.draw(screen)
+                                    board.select(x,y)
+                                except:
+                                    pass
                     for item in numbers:
                         if event.key == item:
-                            board.slctd_cell.set_sketched_value(0)
-                            board.slctd_cell.draw()
-                            board.slctd_cell.set_sketched_value(numbers[item])
-                            board.slctd_cell.draw()
+                            board.clear()
+                            board.sketch(numbers[item])
                     if event.key == pygame.K_RETURN:
-                        board.slctd_cell.set_cell_value(board.slctd_cell.sketched_value)
-                        board.slctd_cell.draw()
-                        board.slctd_cell.set_sketched_value(0)
-                        board.slctd_cell.draw()
+                        board.place_number(board.slctd_cell.sketched_value)
+                        board.update_board()
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
